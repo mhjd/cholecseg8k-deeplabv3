89db1064723f5dbf851a5bc6c971caba9b9584cb
habib <pbllemb@gmail.com>
2026-05-29 21:33:15 +0200

    Add tinyh overfit smoke test

--- scripts/smoke_train.py
+++ scripts/smoke_train.py
@@ -1,13 +1,14 @@
 import torch
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Subset
 from torchvision.models.segmentation import deeplabv3_resnet50
 from src.dataset import CholecDataset
 
 NUM_CLASSES = 13
 
 dataset = CholecDataset()
+subset_dataset = Subset(dataset, range(6))
 
-data_loader = DataLoader(dataset, batch_size=2, shuffle=False)
+data_loader = DataLoader(subset_dataset, batch_size=2, shuffle=False)
 
 images, masks = next(iter(data_loader))
 print(f"batch image shape : {images.shape}")
@@ -32,22 +33,21 @@ print(f"logits dtype: {logits.dtype}")
 print(f"logits shape : {logits.shape}")
 print(f"loss before : {loss_before.item()}")
 
-# training step
 model.train()
-optimizer.zero_grad()
-
-outputs = model(images)
-logits = outputs["out"]
-loss_train = criterion(logits, masks)
-loss_train.backward()
-optimizer.step()
-print(f"training loss : {loss_train.item()}")
-
-# evaluation
-model.eval()
-with torch.no_grad():
-    outputs = model(images)
-    logits = outputs["out"]
-    loss_after = criterion(logits, masks)
-print(f"loss after: {loss_after.item()}")
-
+# overfit loop
+for i in range(10):
+    # training loop
+    
+    loss_acc = 0.0
+    for images, masks in data_loader:
+        # training step
+        optimizer.zero_grad()
+        
+        outputs = model(images)
+        logits = outputs["out"]
+        loss_train = criterion(logits, masks)
+        loss_train.backward()
+        optimizer.step()
+        loss_acc += loss_train.item()
+    loss = loss_acc / len(data_loader)
+    print(f"average training loss of epoch {i + 1}: {loss}")
